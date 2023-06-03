676e3acc5b21134b2c6e7ecf8551031c1e0c1ae7
rmaero <rmaero@Air-de-Rodrigo.fibertel.com.ar>
2023-06-03 14:25:47 -0300

    LEDs y SW andando ok con programa master

--- TP3/source/procTrama.c
+++ TP3/source/procTrama.c
@@ -12,6 +12,9 @@
 #define BYTE4 4
 #define BYTE5 5
 
+
+
+
 static void accionLed(uint8_t charIdLed, board_ledMsg_enum ledMsg)
 {
 	switch (charIdLed)
@@ -25,80 +28,100 @@ static void accionLed(uint8_t charIdLed, board_ledMsg_enum ledMsg)
 			break;
 	}
 }
+static void procLed(char *buf)
+{
+	char tramaRespuesta[7];
+	switch(buf[BYTE4])
+	{
+		case 'A':
+			accionLed(buf[BYTE3], BOARD_LED_MSG_OFF);
+			sprintf(&tramaRespuesta,":%02d0%cA\n",NUMERO_GRUPO,buf[BYTE3]);
+			break;
+		case 'E':
+			accionLed(buf[BYTE3], BOARD_LED_MSG_ON);
+			sprintf(&tramaRespuesta,":%02d0%cE\n",NUMERO_GRUPO,buf[BYTE3]);
+			break;
+		case 'T':
+			accionLed(buf[BYTE3], BOARD_LED_MSG_TOGGLE);
+			sprintf(&tramaRespuesta,":%02d0%cT\n",NUMERO_GRUPO,buf[BYTE3]);
+			break;
+		default:
+			//TODO DEVOLVER ERROR
+			sprintf(&tramaRespuesta,"ERROR\n");
+			break;
+	}
+	//Responder igual a la peticion
 
+	uart_ringBuffer_envDatos(&tramaRespuesta,sizeof(tramaRespuesta));
+}
 
+static void procSw(char *buf)
+{
+	bool estadoSw;
+	char tramaRespuesta[7];
+	switch(buf[BYTE3])
+	{
+		case '1':
+			estadoSw=board_getSw(BOARD_SW_ID_1);
+			//devolver el estado del SW1
+			if (estadoSw)
+				sprintf(&tramaRespuesta,":%02d11P\n",NUMERO_GRUPO);
+			else
+				sprintf(&tramaRespuesta,":%02d11N\n",NUMERO_GRUPO);
+			break;
+		case '3':
+			estadoSw=board_getSw(BOARD_SW_ID_3);
+			//devolver el estado del SW3
+			if (estadoSw)
+				sprintf(&tramaRespuesta,":%02d13P\n",NUMERO_GRUPO);
+			else
+				sprintf(&tramaRespuesta,":%02d13N\n",NUMERO_GRUPO);
+			break;
+		default:
+			//TODO DEVOLVER ERROR
+			sprintf(&tramaRespuesta,"ERROR\n");
+			break;
+	}
+	uart_ringBuffer_envDatos(&tramaRespuesta,sizeof(tramaRespuesta));
+}
+
+static void procAcc(char *buf)
+{
+	char tramaRespuestaAcc[18];
+	//responder
+	//TODO corregir signo
+	sprintf(&tramaRespuestaAcc,":%02d21+%03d+%03d+%03d\n",NUMERO_GRUPO);
+	uart_ringBuffer_envDatos(&tramaRespuestaAcc,sizeof(tramaRespuestaAcc));
+}
 
 void procTrama(char *buf, int length)
 {
-	//:XX01Y’LF’
-	char tramaRespuesta[7];//maybe unnecesary
-	char tramaRespuestaAcc[18];
-	bool estadoSw;
+	//leds->:XX01Y’LF’
+	char tramaAux[7];
 	char grupoStr[2];
 	grupoStr[BYTE0]=buf[BYTE0];
 	grupoStr[BYTE1]=buf[BYTE1];
 
+
+	//obligadamente le tengo que dar un puntero en el segundo argumento,
 	int grupo;
-	grupo = (int) strtol(grupoStr, tramaRespuesta, 10); //obligadamente le tengo que dar un puntero en el segundo argumento, es basura pero no importa porque lo voy a pisar luego.
+	grupo = (int) strtol(grupoStr, tramaAux, 10);
 
 	if(grupo!=NUMERO_GRUPO)
 		return;
 
-
-
     switch (buf[BYTE2])
     {
 		case '0'://0= acciones sobre LEDs
-			switch(buf[BYTE4])
-			{
-				case 'A':
-					accionLed(buf[BYTE3], BOARD_LED_MSG_OFF);
-					break;
-				case 'P':
-					accionLed(buf[BYTE3], BOARD_LED_MSG_ON);
-					break;
-				case 'T':
-					accionLed(buf[BYTE3], BOARD_LED_MSG_TOGGLE);
-					break;
-				default:
-					//DEVOLVER ERROR
-					break;
-			}
-			//Responder igual a la peticion
-			uart_ringBuffer_envDatos(&buf,sizeof(buf));
+			procLed(buf);
 			break;
 
 		case '1'://1= leer estado SW
-			switch(buf[BYTE4])
-			{
-				case '1':
-					estadoSw=board_getSw(BOARD_SW_ID_1);
-					//devolver el estado del SW1
-					if (estadoSw)
-						sprintf(&tramaRespuesta,":%02d11P\n",NUMERO_GRUPO);
-					else
-						sprintf(&tramaRespuesta,":%02d11N\n",NUMERO_GRUPO);
-					break;
-				case '3':
-					estadoSw=board_getSw(BOARD_SW_ID_3);
-					//devolver el estado del SW3
-					if (estadoSw)
-						sprintf(&tramaRespuesta,":%02d13P\n",NUMERO_GRUPO);
-					else
-						sprintf(&tramaRespuesta,":%02d13N\n",NUMERO_GRUPO);
-					break;
-				default:
-					//DEVOLVER ERROR
-					break;
-			}
-			uart_ringBuffer_envDatos(&tramaRespuesta,sizeof(tramaRespuesta));
+			procSw(buf);
 			break;
 
 		case '2'://2= leer acelerometro
-			//responder
-			//TODO corregir signo
-			sprintf(&tramaRespuestaAcc,":%02d21+%03d+%03d+%03d\n",NUMERO_GRUPO);
-			uart_ringBuffer_envDatos(&tramaRespuestaAcc,sizeof(tramaRespuestaAcc));
+			procAcc(buf);
 			break;
 
     }
